a0e166896d1394006f7e2ab7b38c1f59e5e2e88a
BungusUngus <xavier.katzakis@hotmail.com>
2024-09-06 13:18:47 +1000

    CONFUSION
    RAHHHHHHH

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rb2d;
+    private Vector2 moveInput;
+    private bool facingRight;
+    private bool facingUp;
+
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float blend = Mathf.Pow(0.5f, Time.deltaTime * moveSpeed);
+
+
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        rb2d.velocity = moveInput * moveSpeed;
+    }
+}
